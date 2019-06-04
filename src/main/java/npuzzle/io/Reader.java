db74c0d39ae8d51a9acd5ed51e8ddd325a168c4b
dpozinen <dpozinen@gmail.com>
2019-06-04 17:21:21 +0300

    add basic logic

--- src/main/java/npuzzle/io/Reader.java
+++ src/main/java/npuzzle/io/Reader.java
@@ -1,14 +1,58 @@
 package npuzzle.io;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author dpozinen
  * @author ollevche
  * 
- * used to read program input
+ *         used to read program input
  * 
  */
 
- public class Reader {
+public class Reader {
 
-    // TODO: write reader 
- }
+	private String fullFileName;
+
+	public Reader() {
+	}
+
+	public Reader(String fullFileName) {
+		this.fullFileName = fullFileName;
+	}
+
+	public List<String> read(boolean fromFile) {
+		try {
+			List<String> linesList;
+			if (fullFileName != null) {
+				linesList = readFromFile();
+				validateList(linesList);
+			}
+		} catch (IOException | RuntimeException e) {
+			System.err.println(e.getMessage());
+		}
+		return null;
+	}
+
+	private void validateList(List<String> linesList) throws RuntimeException {
+		for (String s : linesList)
+			validateLine(s);
+	}
+	
+	private void validateLine(String s) {
+		if (s.isEmpty())
+			throw new RuntimeException("Invaild Input");
+	}
+
+	private List<String> readFromFile() throws IOException {
+		return Files.readAllLines(Paths.get(fullFileName), StandardCharsets.UTF_8);
+	}
+
+}
