2b57fbb1583dd8a6473aca922ad8c4ec1830b4ef
dpozinen <dpozinen@gmail.com>
2019-06-04 23:13:42 +0300

    add Utils class

---
+++ src/main/java/npuzzle/utils/Utils.java
@@ -0,0 +1,15 @@
+package npuzzle.utils;
+
+public class Utils {
+	private Utils(){}
+
+	private static int n;
+
+	public static void setN(int setN) {
+		n = setN;
+	}
+
+	public static int getN() {
+		return n;
+	}
+}
