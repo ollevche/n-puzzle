1950826ab6a0bf402e7666bc84086cb73bc87f7b
dpozinen <dpozinen@gmail.com>
2019-06-10 18:12:23 +0300

    add Writer

--- src/main/java/npuzzle/io/Writer.java
+++ src/main/java/npuzzle/io/Writer.java
@@ -1,12 +1,54 @@
 package npuzzle.io;
 
+import npuzzle.logic.State;
+import npuzzle.utils.Utils;
+
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author dpozinen
  * @author ollevche
- * 
+ *
  * used to write program output
- * 
+ *
  */
 
- public class Writer {
- }
+public class Writer {
+
+	public void write(State state) {
+		StringBuilder rows = new StringBuilder();
+		int col = 0;
+
+		Collections.sort(state.getTiles());
+		for (Integer tile : state.getTiles()) {
+			rows.append(String.format("%5s", String.valueOf(tile)));
+			if (++col < Utils.getN()) {
+				rows.append(" ");
+			}
+			else {
+				rows.append("\n");
+				col = 0;
+			}
+		}
+		System.out.println(rows);
+	}
+
+	// TODO: count proper offset
+	public void write(List<State> states) {
+		int i = states.size();
+		int offset = 15;
+		String format = "%" + offset + "s";
+		String arrowFormat = "%" + (offset + 1) + "s";
+
+		for (State state : states) {
+			write(state);
+			if (--i > 0)
+			{
+				System.out.println(String.format(format, "|"));
+				System.out.println(String.format(format, "|"));
+				System.out.println(String.format(arrowFormat, "\\ /"));
+			}
+		}
+	}
+}
