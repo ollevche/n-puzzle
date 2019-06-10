9bd645bef8cec37b264f54874f9feff7f571bec9
dpozinen <dpozinen@gmail.com>
2019-06-10 18:17:08 +0300

    re-indent with tabs

--- src/main/java/npuzzle/App.java
+++ src/main/java/npuzzle/App.java
@@ -1,7 +1,20 @@
 package npuzzle;
 
+import npuzzle.io.Reader;
+import npuzzle.io.Writer;
+import npuzzle.logic.State;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("42");
-    }
+	public static void main(String[] args) {
+		State startingState = new Reader("C:\\Users\\User\\Desktop\\Code\\Reports\\STATE.txt").read();
+		List<State> states = new ArrayList<>();
+		states.add(startingState);
+
+		for (int i = 0; i < 10; i++)
+			states.add(new State(startingState));
+		new Writer().write(states);
+	}
 }
