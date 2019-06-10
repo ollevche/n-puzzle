431d8a031e2d2d2f361d2755aaf239a6a7233e36
dpozinen <dpozinen@gmail.com>
2019-06-10 18:13:47 +0300

    change to universal reading

--- src/main/java/npuzzle/io/Reader.java
+++ src/main/java/npuzzle/io/Reader.java
@@ -1,9 +1,9 @@
 package npuzzle.io;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import npuzzle.logic.State;
+
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -17,49 +17,46 @@ import java.util.*;
  */
 
 public class Reader {
-
 	private String fullFileName;
-	private List<Integer> tiles = new ArrayList<>();;
-	private Validator validator = new Validator(tiles);
-
-	public Reader() {
-	}
+	private List<Integer> tiles = new ArrayList<>();
 
 	public Reader(String fullFileName) {
 		this.fullFileName = fullFileName;
 	}
 
-	public List<String> read() {
-		List<String> linesList;
-
+	public State read() {
 		try {
-			if (fullFileName != null) {
-				linesList = readFromFile();
-				validator.validateList(linesList);
-			} else {
-				linesList = readFromConsole();
-			}
-			return linesList;
+			readInput();
+			return createStartingState();
 		} catch (IOException e) {
-//			System.err.println(e.getMessage());
-			e.printStackTrace();
+			System.err.println(e.getMessage());
+//			e.printStackTrace();
 		}
-		return Collections.emptyList();
+		return null;
 	}
 
-	private List<String> readFromConsole() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private void readInput() throws IOException {
 		String line;
-		List<String> lineList = new ArrayList<>();
+		Validator validator = new Validator(tiles);
+		InputStream inputStream;
 
-		while ((line = br.readLine()) != null) {
-			validator.validateLine(line);
-			lineList.add(line);
+		if (fullFileName != null)
+			inputStream = new BufferedInputStream(new FileInputStream(fullFileName));
+		else
+			inputStream = System.in;
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+			while ((line = br.readLine()) != null)
+				validator.validateLine(line);
 		}
-		return lineList;
 	}
 
+	@Deprecated
 	private List<String> readFromFile() throws IOException {
 		return Files.readAllLines(Paths.get(fullFileName), StandardCharsets.UTF_8);
+	}
+
+	private State createStartingState() {
+		return new State(tiles);
 	}
 }
