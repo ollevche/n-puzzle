40ca156f4957197a01226fac5044a2fc65386a8c
dpozinen <dpozinen@gmail.com>
2019-06-15 14:34:46 +0300

    add Evaluator

---
+++ src/main/java/npuzzle/logic/Evaluator.java
@@ -0,0 +1,17 @@
+package npuzzle.logic;
+
+public class Evaluator {
+
+	public static int evaluateManhattan() {
+		return 0;
+	}
+
+	public static int evaluateGreedy() {
+		return 0;
+	}
+
+	public static int evaluateUniform() {
+		return 0;
+	}
+
+}
--- src/main/java/npuzzle/logic/State.java
+++ src/main/java/npuzzle/logic/State.java
@@ -1,20 +1,42 @@
 package npuzzle.logic;
 
+import npuzzle.utils.Contstants;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class State
+public class State implements Comparable<State>
 {
 	private List<Integer> tiles;
+	private String mode;
 
 	public State(){}
 
 	public State(State other) {
-		this.tiles = new ArrayList<>(other.tiles);
+		tiles = new ArrayList<>(other.tiles);
+		mode = other.mode;
 	}
 
 	public State(List<Integer> tiles) {
 		this.tiles = tiles;
+	}
+
+//	TODO: these modes should be replaced by 3 heuristic functions. Contstants should determine steps higher
+	public int evaluate() {
+		switch (mode) {
+			case Contstants.MANHATTAN :
+				return Evaluator.evaluateManhattan();
+			case Contstants.GREEDY :
+				return Evaluator.evaluateGreedy();
+			case Contstants.UNIFORM :
+				return Evaluator.evaluateUniform();
+		}
+		return 0;
+	}
+
+	@Override
+	public int compareTo(State o) {
+		return evaluate() - o.evaluate();
 	}
 
 	public List<Integer> getTiles() {
