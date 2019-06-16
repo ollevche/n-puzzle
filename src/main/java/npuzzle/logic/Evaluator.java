4a3da55f60ed4214fc945de9b6367312163de99b
dpozinen <dpozinen@gmail.com>
2019-06-16 15:04:18 +0300

    add FunctionalInterface

--- src/main/java/npuzzle/logic/Evaluator.java
+++ src/main/java/npuzzle/logic/Evaluator.java
@@ -1,17 +1,37 @@
 package npuzzle.logic;
 
+import npuzzle.utils.Constants;
+
+//	TODO: these modes should be replaced by 3 heuristic functions. Constants should determine steps higher
 public class Evaluator {
 
-	public static int evaluateManhattan() {
+	public static int evaluateManhattan(State state) {
+		return state.getTiles().size();
+	}
+
+	public static int evaluateGreedy(State state) {
 		return 0;
 	}
 
-	public static int evaluateGreedy() {
+	public static int evaluateUniform(State state) {
 		return 0;
 	}
 
-	public static int evaluateUniform() {
-		return 0;
+	@FunctionalInterface
+	public interface EvaluateInterface{
+		int evaluate(State state);
+	}
+
+	public static EvaluateInterface getInterface(String s) {
+		switch (s) {
+			case Constants.MANHATTAN :
+				return Evaluator::evaluateManhattan;
+			case Constants.GREEDY :
+				return Evaluator::evaluateGreedy;
+			case Constants.UNIFORM :
+				return Evaluator::evaluateUniform;
+		}
+		return null;
 	}
 
 }
--- src/main/java/npuzzle/logic/State.java
+++ src/main/java/npuzzle/logic/State.java
@@ -5,33 +5,29 @@ import npuzzle.utils.Constants;
 import java.util.ArrayList;
 import java.util.List;
 
-public class State implements Comparable<State>
-{
-	private List<Integer> tiles;
+public class State implements Comparable<State> {
 	private String mode;
+	private List<Integer> tiles;
+	private final Evaluator.EvaluateInterface evaluator;
 
-	public State(){}
-
-	public State(State other) {
-		tiles = new ArrayList<>(other.tiles);
-		mode = other.mode;
+	public State(String mode) {
+		this.mode = mode;
+		evaluator = Evaluator.getInterface(mode);
 	}
 
 	public State(List<Integer> tiles) {
+		this(Constants.ASTAR);
 		this.tiles = tiles;
 	}
 
-//	TODO: these modes should be replaced by 3 heuristic functions. Constants should determine steps higher
+	public State(State other) {
+		this.mode = other.mode;
+		this.evaluator = other.evaluator;
+		this.tiles = new ArrayList<>(other.tiles);
+	}
+
 	public int evaluate() {
-		switch (mode) {
-			case Constants.MANHATTAN :
-				return Evaluator.evaluateManhattan();
-			case Constants.GREEDY :
-				return Evaluator.evaluateGreedy();
-			case Constants.UNIFORM :
-				return Evaluator.evaluateUniform();
-		}
-		return 0;
+		return evaluator.evaluate(this);
 	}
 
 	@Override
