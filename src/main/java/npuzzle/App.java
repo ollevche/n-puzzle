85061e4c2b3628ed1b8eaa619aac6a05629ad9d2
dpozinen <dpozinen@gmail.com>
2019-07-04 17:38:24 +0300

    add good timetracking

--- src/main/java/npuzzle/App.java
+++ src/main/java/npuzzle/App.java
@@ -1,5 +1,9 @@
 package npuzzle;
 
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
+
 import npuzzle.io.Input;
 import npuzzle.io.Reader;
 import npuzzle.logic.Evaluator;
@@ -8,6 +12,7 @@ import npuzzle.logic.Npuzzle;
 public class App {
 
 	public static void main(String[] args) {
+		Stopwatch stopwatch = Stopwatch.createStarted();
 
 		if (!Reader.readInput(args))
 			return;
@@ -18,7 +23,7 @@ public class App {
 		if (!Npuzzle.execute())
 			return;
 
-		System.out.println("Executed successfully.");
+		System.out.println("Executed successfully. Elapsed:" + stopwatch.elapsed(TimeUnit.MICROSECONDS));
 	}
 
 }
