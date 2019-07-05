502d6564df0f5a0a832259ace208cf0c5b0fa1ba
dpozinen <dpozinen@gmail.com>
2019-07-05 23:14:28 +0300

    unifrom -> uniform

--- src/main/java/npuzzle/utils/Constants.java
+++ src/main/java/npuzzle/utils/Constants.java
@@ -3,7 +3,7 @@ package npuzzle.utils;
 public class Constants {
 
 	public static final String GREEDY = "greedy";
-	public static final String UNIFORM = "unifrom";
+	public static final String UNIFORM = "uniform";
 	public static final String ASTAR = "astar";
 
 	public static final String MANHATTAN = "manhattan";
