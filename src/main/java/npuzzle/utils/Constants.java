6198f8107a6101855c268653ecfe520f517a546b
dpozinen <dpozinen@gmail.com>
2019-07-13 16:39:27 +0300

    add description and private constructor

--- src/main/java/npuzzle/utils/Constants.java
+++ src/main/java/npuzzle/utils/Constants.java
@@ -2,21 +2,28 @@ package npuzzle.utils;
 
 public class Constants {
 
+	private Constants() {
+		throw new AssertionError();
+	}
+
+//	algorithms
 	public static final String GREEDY = "greedy";
 	public static final String UNIFORM = "uniform";
 	public static final String ASTAR = "astar";
 
+//	heuristics
 	public static final String MANHATTAN = "manhattan";
 
+//	options
 	public static final String ALGORITHM = "algorithm";
 	public static final String HEURISTIC = "heuristic";
 	public static final String FILE = "file";
 	public static final String RANDOM = "random";
 
-	// TODO: better descriptions
-	public static final String ALGORITHM_DESCRIPTION = "Algorithm type to use.";
+	// option descriptions
+	public static final String ALGORITHM_DESCRIPTION = "Algorithm to use.";
 	public static final String HEURISTIC_DESCRIPTION = "Heuristic function to use.";
-	public static final String FILE_DESCRIPTION = "File to use as source of input.";
+	public static final String FILE_DESCRIPTION = "File to use as input.";
 	public static final String RANDOM_DESCRIPTION = "Use random input.";
 
 }
