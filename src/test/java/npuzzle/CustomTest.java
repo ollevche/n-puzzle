0cdaa3588f4d244c0b5722fc542d1cd8f2731a6c
dpozinen <dpozinen@gmail.com>
2019-09-07 17:23:38 +0300

    add custom test and testing from a file

--- src/test/java/npuzzle/CustomTest.java
+++ src/test/java/npuzzle/CustomTest.java
@@ -1,11 +1,13 @@
 package npuzzle;
 
+import npuzzle.utils.Constants;
 import org.junit.jupiter.api.Test;
 
 class CustomTest {
 
 	@Test void customTest() {
-//		TestUtils.testRandom(3, 3, );
+        TestUtils.testFromFile("src/test/resources/testCases/validation/lineCommentSequence.txt",
+                Constants.UNIFORM, Constants.MANHATTAN, 1);
 	}
 
 }
--- src/test/java/npuzzle/TestUtils.java
+++ src/test/java/npuzzle/TestUtils.java
@@ -15,7 +15,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class TestUtils {
 
@@ -40,27 +40,39 @@ class TestUtils {
         reader.readTiles();
     }
 
+    static void testFromFile(String file,String a, String h, long minTotalTimeout) {
+        String[] args = String.format("-f %s -a %s -h %s", file, a, h).split(" ");
+        runTest(1, minTotalTimeout, args);
+    }
 
     static void testRandom(int times, int n, String a, String h, long minTotalTimeout) {
+        String[] args = String.format("-r %d -a %s -h %s", n, a, h).split(" ");
+        runTest(times, minTotalTimeout, args);
+    }
+
+    private static void runTest(int times, long minTotalTimeout, String... args) {
         List<Npuzzle> puzzles = new ArrayList<>();
         List<Executable> list = new ArrayList<>();
-        String[] args = String.format("-r %d -a %s -h %s", n, a, h).split(" ");
 
         for (int i = 0; i < times; i++) puzzles.add(Npuzzle.create(new Input(args)));
 
         ExecutorService service = Executors.newFixedThreadPool(times);
         try {
             for (Future<Output> future : service.invokeAll(puzzles, minTotalTimeout, TimeUnit.MINUTES)) {
-                State received = future.get().getFinal();
+                Output output = future.get();
+                int n = output.getInput().getN();
+                State received = output.getFinal();
                 State expected = finals.get(n);
-                list.add(() -> assertEquals(expected, received, wrongFinal(future.get().getInput(), future.get())));
+                list.add(() -> assertFalse(n == 0));
+                list.add(() -> assertEquals(expected, received, wrongFinal(output.getInput(), output)));
             }
         } catch (InterruptedException | ExecutionException ex) {
             ex.printStackTrace();
         } catch (CancellationException e) {
             System.err.printf("Killed: took too long.%n%n");
             System.err.printf("Puzzles were%n%s%n", puzzles);
-            for (Npuzzle p : puzzles) logToJson(p, "timeout");
+            for (Npuzzle p : puzzles) logToJson(p, "Timeout");
+            fail("Killed: took too long");
         } finally {
             service.shutdown();
         }
@@ -68,7 +80,7 @@ class TestUtils {
     }
 
     private static Supplier<String> wrongFinal(Input input, Output output) {
-        logToJson(input, output, "Wrong final");
+        logToJson(input, output, "WrongFinal");
         return () -> "WRONG FINAL";
     }
 
